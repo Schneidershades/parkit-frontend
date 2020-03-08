555163b8f5386856d405a629e217732190a8aedc
schneidershades <schneidershades@gmail.com>
2020-03-08 23:53:10 +0100

    remove white spaces

--- src/boot/my-custom-main.js
+++ src/boot/my-custom-main.js
@@ -1,12 +1,5 @@
 import Vue from 'vue'
-// import App from './App.vue'
-// import router from './router'
-// import store from './store'
 import axios from 'axios'
-
-// require('./store/subscriber')
-
-
 import App from 'app/src/App.vue'
 
 
@@ -14,12 +7,6 @@ import App from 'app/src/App.vue'
 export default async ({ app, router, store, Vue }) => {
   	// something to do
   	axios.defaults.baseURL= 'http://127.0.0.1:8000/api'
-
-	// Vue.config.productionTip = false
-
-	// console.log(localStorage.getItem('token'))
-	// store.dispatch('auth/attempt', localStorage.getItem('token'))
-
 	store.dispatch('auth/attempt', localStorage.getItem('token')).then(() => {
 		new Vue({
 			router,
