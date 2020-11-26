de90be26594c14d31195ab1d4f45e60449b28e0d
Schneider Shades <schneidershades@gmail.com>
2020-11-26 17:39:36 +0100

    cej

---
+++ src/pages/WebAdmin/Employees/AllEmployees.vue
@@ -0,0 +1,28 @@
+<template>
+	<q-page padding>
+      	<div class="q-pa-md q-gutter-sm">
+	      	<q-breadcrumbs>
+		        <q-breadcrumbs-el label="Home" />
+		        <q-breadcrumbs-el label="Employees"/>
+	      	</q-breadcrumbs>
+	    </div>
+
+	    <div class="q-pa-md">
+	        <div class="q-gutter-y-md">
+	            <AllEmployees />
+	        </div>
+	    </div>
+  	</q-page>
+</template>
+
+
+<script>
+
+import AllEmployees from 'components/WebAdmin/Employees/AllEmployees.vue'
+
+export default {
+	components: {
+		AllEmployees
+	}
+}
+</script>
---
+++ src/pages/WebAdmin/Employees/LocationEmployee.vue
@@ -0,0 +1,28 @@
+<template>
+	<q-page padding>
+      	<div class="q-pa-md q-gutter-sm">
+	      	<q-breadcrumbs>
+		        <q-breadcrumbs-el label="Home" />
+		        <q-breadcrumbs-el label="Employees"/>
+	      	</q-breadcrumbs>
+	    </div>
+
+	    <div class="q-pa-md">
+	        <div class="q-gutter-y-md">
+	            <LocationEmployee />
+	        </div>
+	    </div>
+  	</q-page>
+</template>
+
+
+<script>
+
+import LocationEmployee from 'components/WebAdmin/Employees/LocationEmployee.vue'
+
+export default {
+	components: {
+		LocationEmployee
+	}
+}
+</script>
