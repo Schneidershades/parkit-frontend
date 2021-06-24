146c860b1ba666daaafb6327e723b433d1db65b4
Schneider Shades <schneidershades@gmail.com>
2021-06-24 01:44:17 +0100

    remove

--- src/router/middleware/auth.js
+++ src/router/middleware/auth.js
@@ -1,9 +1,9 @@
 export default function auth({ next, store }){
- 	if(store.state.auth.user == null){
-		return next({
-			name: 'adminLogin'
-		})
-	}
+  if(store.state.auth.user == null){
+    return next({
+      name: 'adminLogin'
+    })
+  }
 
-	return next()
+  return next()
 }
